0fd7cd7eed4a9e642844d6dc03defb16f4b4ba9c
harushimo <hvedam@gmail.com>
2016-11-13 23:04:00 -0600

    removed valid ratings

--- movies/media.py
+++ movies/media.py
@@ -1,10 +1,7 @@
 import webbrowser
 
 class Movie():
-	""" This class provides a way to store movie information"""
-
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
+	
 	def __init__(self, movie_title, poster_image, youtube_trailer):
 		self.title = movie_title
 		self.poster_image_url = poster_image
